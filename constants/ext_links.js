4d4fe77feebdf8da57bf4b6fc7862fa841d0396d
Yosef Ashenafi <yosefashenafi@users.noreply.github.com>
2021-05-22 22:50:33 +0300

    lightpaper link attached

--- constants/ext_links.js
+++ constants/ext_links.js
@@ -1,7 +1,7 @@
 const EXT_LINKS = [
     {
         name: 'Lightpaper',
-        url: '/',
+        url: 'https://docs.google.com/document/d/1k2CvA4iq-8a9WVwzpgKhZB8_0cadnio0/edit?dls=true',
     },
 ];
 
