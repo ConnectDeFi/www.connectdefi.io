f8db676400d7fd1bdae621e85d2635f86d29f1bc
Yosef Ashenafi <yosefashenafi@users.noreply.github.com>
2021-05-25 08:03:48 +0300

    home and who we are section responsivness

--- components/base_page.js
+++ components/base_page.js
@@ -1,11 +1,9 @@
 import Head from 'next/head';
-
 import { Header } from '../components/header';
 import { Footer } from '../components/footer';
 import { CommonCSS } from '../components/css/common';
 import { CommonJS } from '../components/js/common';
 import { NonBlockingJS } from '../components/js/nonblocking';
-
 import css from '../styles/common.module.scss';
 import utilsCss from '../styles/utils.module.scss';
 
@@ -18,7 +16,7 @@ export default function Page({ children }) {
                 <meta charSet="utf-8" />
                 <meta
                     name="viewport"
-                    content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                    content="width=device-width, initial-scale=1, shrink-to-fit=Yes"
                 />
 
                 <link rel="icon" href="/favicon.ico" />
